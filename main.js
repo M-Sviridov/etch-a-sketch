5b1fc5560bcd30bb0ac0980d89467304ed7ab74c
Mathieu Sviridov <msviridov@pm.me>
2021-06-30 16:22:54 +1000

    Random color and shading functionality implemented and fully functional

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const gridContainer = document.querySelector('.grid-container');
 const resetButton = document.getElementById('reset-button');
+const randomButton = document.getElementById('random-button');
+const shadingButton = document.getElementById('shading-button');
 
 // Default grid size
 let size = 16;
@@ -43,6 +45,31 @@ function reset() {
 
 resetButton.addEventListener('click', () => reset());
 
+function randomColor() {
+    const colors = ['#343a40', '#6c757d', '#adb5bd'];
+    const gridItem = document.querySelectorAll('#grid-item');
+    gridItem.forEach(grid => {
+        grid.addEventListener('mouseenter', () => {
+            grid.style.backgroundColor = colors[Math.floor(
+                Math.random() * colors.length)];
+        })
+    });
+}
 
+randomButton.addEventListener('click', () => randomColor());
+
+function shadingColor() {
+    const gridItem = document.querySelectorAll('#grid-item');
+    gridItem.forEach(grid => {
+        let opacity = 0.1
+        grid.addEventListener('mouseenter', () => {
+            grid.style.backgroundColor = '#ca1643';
+            grid.style.opacity = opacity + 0.1;
+            opacity += 0.1;
+        });
+    });
+}
+
+shadingButton.addEventListener('click', () => shadingColor());
 
 
